f378d28f9a8b03b43fe1e5ba0e0ca88903742268
Jossany Moura <jossany.junior@e-unicred.com.br>
2025-04-20 20:45:07 -0300

    chore: implement app in main

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/mjossany/workoutPostingService/internal/app"
+)
 
+func main() {
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("we are running our app")
 }
