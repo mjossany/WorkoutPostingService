28d50b4fa696df4c8cb211227345fdd3d34629e5
Jossany Moura <mjossany@gmail.com>
2025-04-21 15:50:05 -0300

    chore: implement database connection

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,28 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/mjossany/workoutPostingService/internal/api"
+	"github.com/mjossany/workoutPostingService/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	workoutHandler := api.NewWorkoutHandler()
@@ -22,6 +30,7 @@ func NewApplication() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 
 	return app, nil
