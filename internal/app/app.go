1ff55a2556e7ba2e2d8622c99327b3126a6aae4b
Jossany Moura <jossany.junior@e-unicred.com.br>
2025-04-21 14:30:30 -0300

    chore: start workout handler implementation

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,17 +5,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/mjossany/workoutPostingService/internal/api"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}
 
 	return app, nil
