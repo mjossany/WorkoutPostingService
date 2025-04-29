cb271399919ee6e56878342707f984a92ae02331
Jossany Silva <mjossany@gmail.com>
2025-04-29 16:22:19 -0300

    chore: start user handler implementation

---
+++ internal/api/user_handler.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"errors"
+	"log"
+	"regexp"
+
+	"github.com/mjossany/workoutPostingService/internal/store"
+)
+
+type registerUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Bio      string `json:"bio"`
+}
+
+type UserHandler struct {
+	userStore store.UserStore
+	logger    *log.Logger
+}
+
+func NewUserHandler(userStore store.UserStore, logger *log.Logger) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+		logger:    logger,
+	}
+}
+
+func (u *UserHandler) validateRegisterRequest(req *registerUserRequest) error {
+	if req.Username == "" {
+		return errors.New("username is required")
+	}
+
+	if len(req.Username) > 50 {
+		return errors.New("username must be less than 50 characters")
+	}
+
+	if req.Email == "" {
+		return errors.New("email is required")
+	}
+
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(req.Email) {
+		return errors.New("invalid email format")
+	}
+
+	if req.Password == "" {
+		return errors.New("password is required")
+	}
+
+	return nil
+}
