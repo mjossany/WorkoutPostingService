a75b3fe05b4b404c5d0a3dde921ed894b1ce67f8
Jossany Silva <mjossany@gmail.com>
2025-04-29 17:00:38 -0300

    feature: create user

--- internal/api/user_handler.go
+++ internal/api/user_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
+	"net/http"
 	"regexp"
 
 	"github.com/mjossany/workoutPostingService/internal/store"
@@ -78,4 +79,20 @@ func (h *UserHandler) HandleRegisterUser(w http.ResponseWriter, r *http.Request)
 	if req.Bio != "" {
 		user.Bio = req.Bio
 	}
+
+	err = user.PasswordHash.Set(req.Password)
+	if err != nil {
+		h.logger.Printf("ERROR: setting password: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	err = h.userStore.CreateUser(user)
+	if err != nil {
+		h.logger.Printf("ERROR: registering user: %v", err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error": "internal server error"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{"user": user})
 }
