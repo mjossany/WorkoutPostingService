64f9efd25b85f91f68139d3c68fa371458b778b1
Jossany Silva <mjossany@gmail.com>
2025-04-29 15:47:18 -0300

    chore: start implementing user store

---
+++ internal/store/user_store.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+)
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+type User struct {
+	ID           int       `json:"id`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash password  `json:"-"`
+	Bio          string    `json:"bio"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type PostgresUserStore struct {
+	db *sql.DB
+}
+
+func NewPostgresUserStore(db *sql.DB) *PostgresUserStore {
+	return &PostgresUserStore{db: db}
+}
+
+type UserStore interface {
+	CreateUser(*User) error
+	GetUserByUsername(username string) (*User, error)
+	UpdateUser(*User) error
+}
